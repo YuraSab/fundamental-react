f43229cdf026e50dd856d15a2103ec31e6d76c5f
Yura Sabat <yrasabat@gmail.com>
2022-10-06 13:59:26 +0300

    v7

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PostItem from "./PostItem";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 const PostList = ({posts, title, remove}) => {
 
-    if(!posts.length){
+    if (!posts.length) {
         return (
             <h1 style={{textAlign: 'center'}}>No items found!</h1>
         )
@@ -12,11 +13,24 @@ const PostList = ({posts, title, remove}) => {
     return (
         <div>
             <h1 style={{textAlign: "center"}}>{title}</h1>
-            {
-                posts.map((post, index) =>
-                    <PostItem remove={remove} post={post} key={post.id} number={index+1}/>
-                )
-            }
+            <TransitionGroup>
+                {
+                    posts.map((post, index) =>
+                        <CSSTransition
+                            key={post.id}
+                            timeout={500}
+                            classNames="post"
+                        >
+                            <PostItem
+                                remove={remove}
+                                post={post}
+                                // key={post.id}
+                                number={index + 1}
+                            />
+                        </CSSTransition>
+                    )
+                }
+            </TransitionGroup>
         </div>
     );
 };
